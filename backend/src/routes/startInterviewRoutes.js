f988614c34aac4b51264daf2eae314f9b5e02f1b
m7anj <ManjeevT@hotmail.com>
2025-11-14 22:50:15 +0000

    Implementation: Basic WebSocket functionality

--- backend/src/routes/startInterviewRoutes.js
+++ backend/src/routes/startInterviewRoutes.js
@@ -1,14 +1,30 @@
 import express from 'express';
 import { startInterview } from '../services/startInterviewService.js';
 
+const activeSessions = new Map();
+
 export default function setupWebSocketRoutes(app) {
     app.ws('/interview/:id', async (ws, req) => {
 
         console.log('Interview started');
-
         const questions = await startInterview(req.params.id);
 
-        ws.on('message', function(msg){ 
+        const sessionId = `user_${Date.now()}`;
+
+        activeSessions.set(sessionId, {
+            questions: questions,
+            currentQuestionIndex: 0,
+            followups: [],
+            userAnswers: []
+        })
+
+        ws.sessionId = sessionId;
+        
+        ws.on('message', function(msg){
+            
+            const session = activeSessions.get(ws.sessionId);
+            console.log("Current Session: ", session);
+
             ws.send(msg);
         });
 
